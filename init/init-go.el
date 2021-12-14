db1f2d1039ecc62582a4ffcb7d373567b2c7f2ac
rocky <rocky@gmail.com>
2021-12-15 01:12:36 +0800

    remove company-go & add protobuf

--- init/init-go.el
+++ init/init-go.el
@@ -1,9 +1,7 @@
 (require-package 'lsp-mode)
 (require-package 'go-mode)
-(require-package 'company-go)
+(require-package 'yasnippet)
 (require-package 'project)
-
-(add-hook 'go-mode-hook #'lsp-deferred)
 
 (defun project-find-go-module (dir)
   (when-let ((root (locate-dominating-file dir "go.mod")))
@@ -21,14 +19,22 @@
   (add-hook 'before-save-hook #'lsp-organize-imports t t)
   (add-hook 'before-save-hook 'gofmt-before-save nil t)
   (whitespace-toggle-options '(tabs))
-  (set (make-local-variable 'company-backends) '(company-go))
 
   (local-set-key (kbd "C-c C-b") 'pop-tag-mark)
   (local-set-key (kbd "C-c t") 'go-test-current-file)
   (setq tab-width 4))
 
 (add-hook 'project-find-functions #'project-find-go-module)
+(add-hook 'go-mode-hook #'lsp-deferred)
 (add-hook 'go-mode-hook 'go-mode-defaults)
+(add-hook 'go-mode-hook #'yas-minor-mode)
 
+(require-package 'protobuf-mode)
+(defconst protobuf-style
+  '((c-basic-offset . 2)
+    (indent-tabs-mode . nil)))
+
+(add-hook 'protobuf-mode-hook
+          (lambda () (c-add-style "my-style" protobuf-style t)))
 
 (provide 'init-go)
